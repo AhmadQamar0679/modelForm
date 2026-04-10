b2f6dacdf5a80417cbab7e53ee1f54b3edd0a540
Ahmad Qamar <ahmadqamar.rq@gmail.com>
2026-04-10 21:03:12 +0500

    Update forms.py

--- form/forms.py
+++ form/forms.py
@@ -5,7 +5,6 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model=Student
         fields=['name','age','email']
-
     def clean_age(self):
         age=self.cleaned_data.get('age')
         if age<18:
